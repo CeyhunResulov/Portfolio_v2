dfe9a18f7f7a66da8cb94312245ceb8df3578ca2
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-04-14 15:10:59 +0400

    home page de footer hissesindeki dinamikliyi js ile yazdim

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 // text typs
-
-let textFirst="I Love Wordpress"
+let z=0
+let e=0
 let a=-1
-let b=textFirst.length-1
-console.log(typeof(b))
-function firstText(){
+let x=0
+function firstText(textFirst){
+    z+=1
     a=a+1
-    if(a<textFirst.length){
-        document.querySelector(".home__about--first").innerHTML+=textFirst[a]
+    if(a<textFirst[e].length){
+        document.querySelector(".home__about--first").innerHTML+=textFirst[e][a]
     }else{
-        document.querySelector(".home__about--first").innerHTML=""
+        x+=1
+        let textList=[]
+        for(let i=0;i<textFirst[e].length-x;i++){
+            c=textFirst[e][i]
+            textList.push(c)
+        }
+        textList.pop()
+        let y=""
+        for(let i=0;i<textFirst[e].length-x;i++){
+            y+=textFirst[e][i]
+        }
+        document.querySelector(".home__about--first").innerHTML=y
+    }
+    if(x>=textFirst[e].length){
+        if(z>textFirst[e].length && z<3*textFirst[e].length){
+            a=-1
+            x=0
+            e=1
+        }
+        if(z>2*textFirst[e].length && z<4*textFirst[e].length){
+            a=-1
+            x=0
+            e=2
+        }
+        if(z>5*textFirst[e].length){
+            a=-1
+            x=0
+            e=0
+            z=0
+        }
+        
+        
     }
 }
-setInterval(firstText,100)
+
+// setInterval(firstText,100,["I Love"," Wordpress","I Develop Front-End","I Code Cool Websit"])
+
+setInterval(firstText,100,["I Love Wordpress","I Develop Front-End","I Code Cool Websit"])
+
+
+
+
 
