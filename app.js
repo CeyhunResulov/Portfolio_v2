e9ad95971aab239ca22e94e1f0da6cf5f87f8dae
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-05-20 23:48:38 +0400

    protfolionun responsivliyi

--- app.js
+++ app.js
@@ -1,55 +1,4 @@
-// text typs
-let z = 0;
-let e = 0;
-let a = -1;
-let x = 0;
-function firstText(textFirst) {
-  z += 1;
-  a = a + 1;
-  if (a < textFirst[e].length) {
-    document.querySelector(".home__about--first").innerHTML += textFirst[e][a];
-  } else {
-    x += 1;
-    let textList = [];
-    for (let i = 0; i < textFirst[e].length - x; i++) {
-      c = textFirst[e][i];
-      textList.push(c);
-    }
-    textList.pop();
-    let y = "";
-    for (let i = 0; i < textFirst[e].length - x; i++) {
-      y += textFirst[e][i];
-    }
-    document.querySelector(".home__about--first").innerHTML = y;
-  }
-  if (x >= textFirst[e].length) {
-    if (z > textFirst[e].length && z < 3 * textFirst[e].length) {
-      a = -1;
-      x = 0;
-      e = 1;
-    }
-    if (z > 2 * textFirst[e].length && z < 4 * textFirst[e].length) {
-      a = -1;
-      x = 0;
-      e = 2;
-    }
-    if (z > 5 * textFirst[e].length) {
-      a = -1;
-      x = 0;
-      e = 0;
-      z = 0;
-    }
-  }
-}
-
-setInterval(firstText, 100, [
-  "I Love Wordpress  ",
-  "I Develop Front-End  ",
-  "I Code Cool Website  ",
-]);
-
-// get navbar
-
+// navbar open and close
 function openNavbar() {
   document.querySelector(".home__navpage").style.visibility = "visible";
   document.querySelector(".home__navpage").style.zIndex = 100;
@@ -69,29 +18,12 @@ function closeNavbar() {
   document.querySelector(".home__bar").style.visibility = "visible";
 }
 
-// show projects
-
-const projects = document.querySelector(".home__reset");
-const projectsBox = document.querySelector(".home__container--pro");
-const reset = document.querySelector(".home__container--pro i");
-const portrait = document.querySelector(".home__container--portrait");
-const pro = document.querySelector(".home__container--pro");
-const footer = document.querySelector(".home__desc");
-projects.addEventListener("click", showProjects);
-reset.addEventListener("click", hiddenProjects);
-
-function showProjects(e) {
-  if (window.innerWidth > 720) {
-    projectsBox.style.width = 500 + "px";
-  } else {
-    pro.style.animation = "project 1s ease forwards";
-    footer.style.marginTop = "200px";
-  }
+// cursor
+const cursor = document.querySelector(".home__cursor");
+const body = document.querySelector("body");
+body.addEventListener("mousemove", cursorRun);
+function cursorRun(e) {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
 }
-
-function hiddenProjects(e) {
-  projectsBox.style.width = 0;
-  if (window.innerWidth < 720) {
-    pro.style.animation = "backpro 1s ease forwards";
-  }
-}
+setTimeout(cursorRun(e), 5000);
---
+++ home.js
@@ -0,0 +1,76 @@
+// // text typs
+let z = 0;
+let e = 0;
+let a = -1;
+let x = 0;
+function firstText(textFirst) {
+  z += 1;
+  a = a + 1;
+  if (a < textFirst[e].length) {
+    document.querySelector(".home__about--first").innerHTML += textFirst[e][a];
+  } else {
+    x += 1;
+    let textList = [];
+    for (let i = 0; i < textFirst[e].length - x; i++) {
+      c = textFirst[e][i];
+      textList.push(c);
+    }
+    textList.pop();
+    let y = "";
+    for (let i = 0; i < textFirst[e].length - x; i++) {
+      y += textFirst[e][i];
+    }
+    document.querySelector(".home__about--first").innerHTML = y;
+  }
+  if (x >= textFirst[e].length) {
+    if (z > textFirst[e].length && z < 3 * textFirst[e].length) {
+      a = -1;
+      x = 0;
+      e = 1;
+    }
+    if (z > 2 * textFirst[e].length && z < 4 * textFirst[e].length) {
+      a = -1;
+      x = 0;
+      e = 2;
+    }
+    if (z > 5 * textFirst[e].length) {
+      a = -1;
+      x = 0;
+      e = 0;
+      z = 0;
+    }
+  }
+}
+
+setInterval(firstText, 100, [
+  "I Love Wordpress  ",
+  "I Develop Front-End  ",
+  "I Code Cool Website  ",
+]);
+
+// show projects
+
+const projects = document.querySelector(".home__reset");
+const projectsBox = document.querySelector(".home__container--pro");
+const reset = document.querySelector(".home__container--pro i");
+const portrait = document.querySelector(".home__container--portrait");
+const pro = document.querySelector(".home__container--pro");
+const footer = document.querySelector(".home__desc");
+projects.addEventListener("click", showProjects);
+reset.addEventListener("click", hiddenProjects);
+
+function showProjects(e) {
+  if (window.innerWidth > 720) {
+    projectsBox.style.width = 500 + "px";
+  } else {
+    pro.style.animation = "project 1s ease forwards";
+    footer.style.marginTop = "200px";
+  }
+}
+
+function hiddenProjects(e) {
+  projectsBox.style.width = "0";
+  if (window.innerWidth < 720) {
+    pro.style.animation = "backpro 1s ease forwards";
+  }
+}
